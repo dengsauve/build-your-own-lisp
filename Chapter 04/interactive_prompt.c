e1ee74bdb614448ca3f4250a5a9ef23106487d55
dennis.sauve <dennis.sauve@triadhq.com>
2018-07-16 17:50:57 -0700

    Finished Chapter 4

--- Chapter 04/interactive_prompt.c
+++ Chapter 04/interactive_prompt.c
@@ -7,20 +7,20 @@
 int main(int argc, char** argv) {
 
   /* Print Version and Exit Information */
-  puts("Lispy Version 0.0.0.0.1");
-  puts("Press Ctrl+c to Exit\n");
+  puts("Denpy Version 0.0.2");
+  puts("Press Ctrl+c to Exit Denpy\n");
 
   /* In a never ending loop */
   while (1) {
 
     /* Output our prompt and get input */
-    char* input = readline("lispy> ");
+    char* input = readline("denpy> ");
 
     /* Add input to history */
     add_history(input);
 
     /* Echo input back to user */
-    printf("No you're a %s\n", input);
+    printf("Really? I always thought %s was interesting!\n", input);
 
     /* Free retrieved input */
     free(input);
